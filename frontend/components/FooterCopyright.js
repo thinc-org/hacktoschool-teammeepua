24cc0cca398df99e24f4a16c04b69ce1fa765362
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-08 22:11:00 +0700

    feat: create FooterCopyright component

---
+++ frontend/components/FooterCopyright.js
@@ -0,0 +1,10 @@
+export const FooterCopyright = () => {
+  return (
+    <p className="text-xs text-center my-6 font-semibold">
+      ©2021 Thinc. x Cleverse. Project for{" "}
+      <a href="#" className="underline">
+        hack to school
+      </a>
+    </p>
+  );
+};
