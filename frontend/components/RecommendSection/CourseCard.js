28fbb3f5393ef32b1cdcbf514be55c48e3746ef0
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-09 15:53:14 +0700

    fix: add hover effect on CourseCard

--- frontend/components/RecommendSection/CourseCard.js
+++ frontend/components/RecommendSection/CourseCard.js
@@ -1,6 +1,6 @@
 export const CourseCard = (props) => {
   return (
-    <div className="flex flex-col justify-between h-[140px] w-[330px] p-5 bg-stone-100 rounded-2xl shadow-lg">
+    <div className="flex flex-col justify-between h-[140px] w-[330px] p-5 bg-stone-100 rounded-2xl shadow-lg hover:scale-105 hover:shadow- transition">
       <h2 className="text-xl font-bold text-cyan-700">{props.name}</h2>
       <div className="underline text-stone-500">
         <a href="#">Enroll</a>
