c9f85ec16cca8585ccd323180fd76a2bb1d8d20f
thamph <poomz180@gmail.com>
2023-01-09 14:56:19 +0700

    build: setup Go

---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type testStruct struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/api/test_get", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "hello 6432846982141234314",
+		})
+	})
+
+	router.POST("/api/test_post", func(context *gin.Context) {
+		var requestBody testStruct
+		err := context.BindJSON(&requestBody)
+		if err != nil {
+			fmt.Println("error!!!!!!!!!!!!!!!!!!!!!!!")
+			return
+		}
+		requestBody.Name += "  tested"
+		context.JSON(http.StatusOK, requestBody)
+	})
+
+	router.Run("localhost:3030")
+
+}
