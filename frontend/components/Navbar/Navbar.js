6d7c04041fdc97f416e608158fd2602d16b4424b
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-09 20:28:14 +0700

    fix: change Navbar height

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
   const router = useRouter();
 
   return (
-    <nav className="flex items-center justify-between px-40 h-16 w-full bg-stone-100">
+    <nav className="flex items-center justify-between px-40 h-20 w-full bg-stone-100">
       <div className="flex items-center">
         <p className="text-xl font-bold mr-6">Thinc Academy</p>
         <ul className="list-none whitespace-nowrap text-base">
