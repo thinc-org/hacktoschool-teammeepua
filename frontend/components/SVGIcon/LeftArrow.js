3afe7af469fd0abf7ead406223142c3ccb4270d7
bosstnp <boss.32560@gmail.com>
2023-01-10 12:28:38 +0700

    fix: change SVGIcon to react style attribute

--- frontend/components/SVGIcon/LeftArrow.js
+++ frontend/components/SVGIcon/LeftArrow.js
@@ -5,11 +5,11 @@ export default function LeftArrow() {
       width="24"
       height="24"
       fill="currentColor"
-      class="bi bi-arrow-left-short"
+      className="bi bi-arrow-left-short"
       viewBox="0 0 16 16"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
       />
     </svg>
--- frontend/components/SVGIcon/RightArrow.js
+++ frontend/components/SVGIcon/RightArrow.js
@@ -5,11 +5,11 @@ export default function LeftArrow() {
       width="24"
       height="24"
       fill="currentColor"
-      class="bi bi-arrow-right-short"
+      className="bi bi-arrow-right-short"
       viewBox="0 0 16 16"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
       />
     </svg>
