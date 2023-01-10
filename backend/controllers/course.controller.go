1a895128abccf55c072a565f83204b79814f8071
thamph <poomz180@gmail.com>
2023-01-11 01:04:05 +0700

    feat: add create_course api

---
+++ backend/controllers/course.controller.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thamph/thinc-hacktoschool/database"
+	"github.com/thamph/thinc-hacktoschool/models"
+	"log"
+	"net/http"
+)
+
+func CreateCourse(context *gin.Context) {
+	var course models.Course
+	if err := context.BindJSON(&course); err != nil {
+		log.Fatal(err)
+	}
+	database.DB.Db.Create(&course)
+	context.JSON(http.StatusOK, course)
+}
