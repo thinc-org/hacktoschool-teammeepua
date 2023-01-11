8057b3999cfac25c1c923058c9741ff1cd34f7fb
bosstnp <boss.32560@gmail.com>
2023-01-11 15:36:16 +0700

    fix: mobile landing page menu and profile button

--- frontend/components/Navbar/NavbarHam.js
+++ frontend/components/Navbar/NavbarHam.js
@@ -1,6 +1,9 @@
-export const NavbarHam = () => {
+export const NavbarHam = (props) => {
   return (
-    <div class="space-y-1.5 hidden lg:block">
+    <div
+      class="space-y-1.5 hidden lg:block cursor-pointer z-10"
+      onClick={props.onClick}
+    >
       <span class="block w-8 h-0.5 bg-black"></span>
       <span class="block w-5 h-0.5 bg-black"></span>
       <span class="block w-3 h-0.5 bg-black"></span>
--- frontend/components/Navbar/NavbarLogo.js
+++ frontend/components/Navbar/NavbarLogo.js
@@ -4,11 +4,13 @@ import ThincLogo from "../../public/thinc-logo.png";
 
 export const NavbarLogo = () => {
   return (
-    <Link href="/" className="flex lg:absolute lg:left-0 lg:right-0 lg:justify-center">
-      <Image src={ThincLogo} width={32} height={32} />
-      <p className="text-xl lg:text-lg sm:hidden font-bold mx-6 tracking-tight">
-        Thinc Academy
-      </p>
-    </Link>
+    <div className="flex lg:absolute lg:left-0 lg:right-0 lg:justify-center">
+      <Link href="/" className="flex">
+        <Image src={ThincLogo} width={32} height={32} />
+        <p className="text-xl lg:text-lg sm:hidden font-bold mx-6 tracking-tight">
+          Thinc Academy
+        </p>
+      </Link>
+    </div>
   );
 };
