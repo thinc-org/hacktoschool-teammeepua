b7e38ccfa74bd8a7f709ccccc72b50b2ea038eb8
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-11 15:24:31 +0700

    fix: responsive width of recommend card

--- frontend/components/RecommendSection/RecommendCard.js
+++ frontend/components/RecommendSection/RecommendCard.js
@@ -1,6 +1,6 @@
 export const RecommendCard = (props) => {
   return (
-    <div className="flex flex-col justify-between h-[160px] sm:h-[140px] max-w-[450px] w-[30%] sm:max-w-[80%] sm:w-[400px] sm:my-5 p-5 bg-stone-100 rounded-2xl shadow-lg hover:scale-105 hover:shadow- transition">
+    <div className="flex flex-col items-center justify-between h-[160px] sm:h-[140px] w-[30%] sm:w-[50%] sm:min-w-[250px] sm:my-5 p-5 bg-stone-100 rounded-2xl shadow-lg hover:scale-105 hover:shadow- transition">
       <h2 className="text-2xl lg:text-xl sm:text-base font-bold text-cyan-700 sm:text-center">
         {props.name}
       </h2>
