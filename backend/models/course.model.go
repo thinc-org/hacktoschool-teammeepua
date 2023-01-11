764d79999f814cc409754924204614379e30e98d
thamph <poomz180@gmail.com>
2023-01-11 21:22:20 +0700

    fix: change json format

--- backend/models/course.model.go
+++ backend/models/course.model.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Course struct {
 	gorm.Model
 	Title        string `json:"title"`
-	CoverURL     string `json:"cover_url"`
+	CoverURL     string `json:"coverURL"`
 	Description  string `json:"description"`
 	InstructorID uint
 }
--- backend/models/user.model.go
+++ backend/models/user.model.go
@@ -6,8 +6,8 @@ type User struct {
 	gorm.Model
 	Email       string `json:"email"`
 	Password    string `json:"password"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	DisplayName string `json:"display_name"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	DisplayName string `json:"displayName"`
 	Role        string `json:"role"`
 }
