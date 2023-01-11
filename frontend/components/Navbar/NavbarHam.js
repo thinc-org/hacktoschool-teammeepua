7b620822a1c7e930af5c1ba46a2f34d61d001e49
bosstnp <boss.32560@gmail.com>
2023-01-11 21:58:04 +0700

    fix: typo and object key

--- frontend/components/Navbar/NavbarHam.js
+++ frontend/components/Navbar/NavbarHam.js
@@ -1,7 +1,7 @@
 export const NavbarHam = (props) => {
   return (
     <div
-      class="space-y-1.5 hidden lg:block cursor-pointer z-10"
+      className="space-y-1.5 hidden lg:block cursor-pointer z-10"
       onClick={props.onClick}
     >
       <span class="block w-8 h-0.5 bg-black"></span>
