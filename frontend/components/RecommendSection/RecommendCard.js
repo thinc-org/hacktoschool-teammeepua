8413d5f7c7a7c6049853cad731ff53b913ddbbb4
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-11 15:06:58 +0700

    feat: add arrow icon

--- frontend/components/RecommendSection/RecommendCard.js
+++ frontend/components/RecommendSection/RecommendCard.js
@@ -4,11 +4,19 @@ export const RecommendCard = (props) => {
       <h2 className="text-2xl lg:text-xl sm:text-base font-bold text-cyan-700 sm:text-center">
         {props.name}
       </h2>
-      <div className="underline text-stone-500 sm:text-center">
+      <div className="underline text-stone-500 sm:text-center flex items-center group">
         <a className="text-lg md:text-base sm:text-sm" href="#">
           Enroll
         </a>
-        {/* insert icon here */}
+
+        <svg
+          className="scale-[.40] relative right-2 translate-x-0 group-hover:translate-x-2 transition-transform"
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          width="48"
+        >
+          <path d="m35.1 32.95-2.15-2.1 5.3-5.3H6v-3h32.3l-5.3-5.3 2.1-2.1 8.9 8.9Z" />
+        </svg>
       </div>
     </div>
   );
