d7a0e3828cf197df68d0814fbbb3e8f25698ee02
bosstnp <boss.32560@gmail.com>
2023-01-11 18:03:55 +0700

    style: add sliding animation to mobile nav menu

--- frontend/components/Navbar/NavbarMenuMobile.js
+++ frontend/components/Navbar/NavbarMenuMobile.js
@@ -1,27 +1,60 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import X from "../SVGIcon/X";
 
 export const NavbarMenuMobile = ({ visible, onExit }) => {
-  if (visible)
-    return (
-      <div className="absolute left-0 top-0 w-screen h-screen z-10 flex flex-row bg-black bg-opacity-50">
+  return (
+    <>
+      <div
+        className={`
+          absolute left-0 top-0 w-screen h-screen z-10 flex flex-row
+          ease-in-out transition-all duration-200
+          ${visible ? "translate-x-0" : "-translate-x-full"}
+        `}
+      >
         <div className="bg-white w-[500px] h-full">
-          <ul
-            className="list-none whitespace-nowrap text-base flex flex-col gap-16 pl-10 pt-10"
+          <div
+            className="flex justify-start w-min mx-10 mt-8 mb-6 cursor-pointer hover:text-stone-400"
             onClick={onExit}
           >
-            <li className="inline">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="inline">
-              <Link href="/dashboard">Dashboard</Link>
-            </li>
-            <li className="inline">
-              <Link href="/browse/1">Courses</Link>
-            </li>
+            <X />
+          </div>
+
+          <ul
+            className="list-none whitespace-nowrap text-base flex flex-col"
+            onClick={onExit}
+          >
+            <ItemTab href="/">Home</ItemTab>
+            <ItemTab href="/dashboard">Dashboard</ItemTab>
+            <ItemTab href="/browse/1">Courses</ItemTab>
           </ul>
         </div>
 
+        {/* Empty right space for exiting the menu */}
         <div className="w-full h-full" onClick={onExit} />
       </div>
-    );
+
+      {/* Black screen behind */}
+      <div
+        className={`
+        absolute left-0 top-0 w-screen h-screen bg-black bg-opacity-50
+        ${visible ? "" : "hidden"}
+      `}
+      />
+    </>
+  );
+};
+
+const ItemTab = (props) => {
+  const router = useRouter();
+
+  return (
+    <li
+      className="hover:bg-stone-200 w-full pl-10 py-5 ease-in-out transition-all cursor-pointer"
+      onClick={() => router.push(props.href)}
+    >
+      {/* <Link href={props.href}>{props.children}</Link> */}
+      {props.children}
+    </li>
+  );
 };
