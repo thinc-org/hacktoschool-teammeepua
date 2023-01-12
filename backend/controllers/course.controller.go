2a7448200da15e30891be8695b85c2fbe5c413fb
Vorapon Kachapornkul <hakao97@gmail.com>
2023-01-12 15:58:32 +0700

    fix: change search to return all if empty

--- backend/controllers/course.controller.go
+++ backend/controllers/course.controller.go
@@ -18,22 +18,18 @@ func CreateCourse(context *gin.Context) {
 	context.JSON(http.StatusOK, course)
 }
 
-func GetAllCourse(context *gin.Context) {
-	var courses []models.Course
-	database.DB.Db.Find(&courses)
-	context.JSON(http.StatusOK, courses)
-}
-
-func SearchCourseById(context *gin.Context) {
+func SearchCourse(context *gin.Context) {
 	var courses []models.Course
 	id := context.Param("id")
-	database.DB.Db.Where("id = ?", id).Find(&courses)
-	context.JSON(http.StatusOK, courses)
-}
-
-func SearchCourseByName(context *gin.Context) {
-	var courses []models.Course
 	name := context.Param("name")
-	database.DB.Db.Where("title LIKE ?", "%"+name+"%").Find(&courses)
+	if id != "" {
+		database.DB.Db.Where("id = ?", id).Find(&courses)
+	}
+	if name != "" {
+		database.DB.Db.Where("title LIKE ?", "%"+name+"%").Find(&courses)
+	}
+	if id == "" && name == "" {
+		database.DB.Db.Find(&courses)
+	}
 	context.JSON(http.StatusOK, courses)
 }
