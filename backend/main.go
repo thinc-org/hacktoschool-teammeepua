9d568950300f075d0adad87fd9824ff165f48e4e
Vorapon Kachapornkul <hakao97@gmail.com>
2023-01-12 15:44:52 +0700

    fix: cors policy on go

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/thamph/thinc-hacktoschool/controllers"
 	"github.com/thamph/thinc-hacktoschool/database"
@@ -10,6 +11,11 @@ func main() {
 	database.ConnectDatabase()
 
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"}
+	router.Use(cors.New(config))
 
 	router.POST("/api/create_course", controllers.CreateCourse)
 	router.GET("/api/get_all_courses", controllers.GetAllCourse)
