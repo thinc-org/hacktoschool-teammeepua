cb5b1798dab911fa000b175cc85192b9e080bc3a
bosstnp <boss.32560@gmail.com>
2023-01-12 10:46:13 +0700

    fix: className typo

--- frontend/components/Navbar/NavbarHam.js
+++ frontend/components/Navbar/NavbarHam.js
@@ -4,9 +4,9 @@ export const NavbarHam = (props) => {
       className="space-y-1.5 hidden lg:block cursor-pointer z-10"
       onClick={props.onClick}
     >
-      <span class="block w-8 h-0.5 bg-black"></span>
-      <span class="block w-5 h-0.5 bg-black"></span>
-      <span class="block w-3 h-0.5 bg-black"></span>
+      <span className="block w-8 h-0.5 bg-black"></span>
+      <span className="block w-5 h-0.5 bg-black"></span>
+      <span className="block w-3 h-0.5 bg-black"></span>
     </div>
   );
 };
