46e5b756b4e77d132ef137cb9803d63e262ea347
Vorapon Kachapornkul <hakao97@gmail.com>
2023-01-13 21:57:47 +0700

    feat: create example rte

--- frontend/components/TextEditor.js
+++ frontend/components/TextEditor.js
@@ -1,10 +1,35 @@
-import { useState } from 'react';
-import { RichTextEditor } from '@mantine/rte';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
-const initialValue =
-  '<p>Your initial <b>html value</b> or an empty string to init editor without value</p>';
 
-export default function TextEditor () {
-  const [value, onChange] = useState(initialValue);
-  return <RichTextEditor value={value} onChange={onChange} id="rte" />;
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return (
+    <div>
+        <Editor apiKey='tyd90e7cd7pitroji54zc5y2g2wm4w6ng3v5pjvz2in0ur87'
+        onInit={(evt, editor) => editorRef.current = editor}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          selector: 'textarea#basic-example',
+  height: 500,
+  plugins: [
+    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+    'insertdatetime', 'media', 'table', 'help', 'wordcount'
+  ],
+  toolbar: 'undo redo | blocks | ' +
+  'bold italic backcolor | alignleft aligncenter ' +
+  'alignright alignjustify | bullist numlist outdent indent | ' +
+  'removeformat | help',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+      
+}}/>
+      <button onClick={log}>Log editor content</button>
+      </div>
+  );
 }
