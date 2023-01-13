0ed9e4ac75937b00b07cc40203f9bfa6f9acc498
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-13 14:55:46 +0700

    feat: student course pop-up component

--- frontend/components/Button.js
+++ frontend/components/Button.js
@@ -4,7 +4,7 @@ export const Button = (props) => {
       className="rounded-full bg-cyan-700 hover:bg-cyan-900 px-5 py-2 text-white text-lg font-bold"
       onClick={props.onClick}
     >
-      {props.children}
+      {props.name}
     </button>
   );
 };
