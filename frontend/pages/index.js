0f3e171513506da0b26fcac59f328a34b01cf2ff
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-01-13 16:04:20 +0700

    feat: new edit course page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,14 +2,16 @@ import { FeatureSection } from "../components/FeatureSection/FeatureSection";
 import { Footer } from "../components/Footer/Footer";
 import { RecommendSection } from "../components/RecommendSection/RecommendSection";
 import { Banner } from "../components/Banner/Banner";
+import { EditCourse } from "../components/EditCourse.js/EditCourse";
 
 export default function Home() {
   return (
     <>
-      <Banner />
+      <EditCourse />
+      {/* <Banner />
       <RecommendSection />
       <FeatureSection />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
